d4d38245298a7e16533c5968549e93510eda9167
daniyalAhmed-code <70039650+daniyalAhmed-code@users.noreply.github.com>
2020-10-01 03:55:46 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-const app = require('./app');
 const http = require('http');
 
-http.createServer(app).listen(process.env.PORT);
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
