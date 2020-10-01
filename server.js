04f2e5c3450f820f051b76b4d099f8bf993d9d3b
Apple <apple@Apples-iMac.local>
2020-10-01 23:07:29 +0500

    Changing in server.js file

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
-const app = require('./app');
-const http = require('http');
 
-http.createServer(app).listen(process.env.PORT);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
