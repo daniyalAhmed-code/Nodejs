96128d9c1c50778940ec0239413b38d97973066a
Daniyal <daniyal@eurustechnologies.com>
2020-10-02 02:02:58 +0500

    Added Console log

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const port = 3030
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
-console.log(process.env.PORT)
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
