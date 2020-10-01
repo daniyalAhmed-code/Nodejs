25698a951c94f82a2d63df884968948c2fe34a2d
Daniyal <daniyal@eurustechnologies.com>
2020-10-02 02:55:27 +0500

    Added Console log

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3100
 
 app.get('/', (req, res) => {
-  res.send('Hello Daniyal!')
+  res.send('Hello Daniyal from the slack bot, Thank you for giving me life!')
 })
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
